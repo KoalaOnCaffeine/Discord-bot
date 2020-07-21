1cf5fe8509f9c0c364b30beaa6e3c49f89ee7e66
KoalaOnCaffeine <tomtomnewton2005@gmail.com>
2020-07-22 00:30:34 +0100

    Updated CommandRegistry parameter name from commandRegister to commandRegistry

--- src/main/java/me/tomnewton/discordbot/listeners/CommandListener.kt
+++ src/main/java/me/tomnewton/discordbot/listeners/CommandListener.kt
@@ -1,14 +1,13 @@
 package me.tomnewton.discordbot.listeners
 
 import me.tomnewton.discordbot.commands.Command
-import me.tomnewton.discordbot.commands.CommandRegister
+import me.tomnewton.discordbot.commands.CommandRegistry
 import me.tomnewton.discordbot.commands.arguments.Arguments
 import net.dv8tion.jda.core.entities.ChannelType
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
-import java.math.BigInteger
 
-class CommandListener(private val prefix: String, private val commandRegister: CommandRegister) : ListenerAdapter() {
+class CommandListener(private val prefix: String, private val commandRegistry: CommandRegistry) : ListenerAdapter() {
 
     private val argumentPattern = Regex(" +")
 
@@ -16,8 +15,8 @@ class CommandListener(private val prefix: String, private val commandRegister: C
         if (!event.message.contentRaw.startsWith(prefix)) return
         val parts = event.message.contentRaw.split(argumentPattern)
         val commandName = parts[0].substring(prefix.length)
-        if (!commandRegister.hasCommand(commandName)) return
-        val command: Command = commandRegister.getCommand(commandName)!!
+        if (!commandRegistry.contains(commandName)) return
+        val command: Command = commandRegistry.get(commandName)!!
         if (event.channelType == ChannelType.PRIVATE && !command.usableInDM) return
         val arguments: Arguments<String> = if (parts.size == 1)
             Arguments(emptyList()) else Arguments(parts.subList(1, parts.size))
