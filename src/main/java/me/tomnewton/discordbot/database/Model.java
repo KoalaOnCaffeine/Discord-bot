11fa188e75318d5d7535616cf6559bc98dd77697
KoalaOnCaffeine <tomtomnewton2005@gmail.com>
2020-07-29 14:17:20 +0100

    Added JavaDocs and a getString method

--- src/main/java/me/tomnewton/discordbot/database/Model.java
+++ src/main/java/me/tomnewton/discordbot/database/Model.java
@@ -27,6 +27,32 @@ public interface Model {
     Object get(String string);
 
     /**
+     * A method to get some value from the model as a String
+     *
+     * @param string The path of the object to get
+     *
+     * @return The found object as a String, else <code>"null"</code>
+     *
+     * @see Model#get(String)
+     * @see Object#toString()
+     */
+
+    default String getString(String string) {
+        final Object value = get(string);
+        if (value == null) return "null";
+        return value.toString();
+    }
+
+    /**
+     * A method to set a string path to a value
+     *
+     * @param string The path of the object
+     * @param value  The value to set it to
+     */
+
+    void set(String string, Object value);
+
+    /**
      * A method to get all of the values in the model
      *
      * @return All of the values in a map
