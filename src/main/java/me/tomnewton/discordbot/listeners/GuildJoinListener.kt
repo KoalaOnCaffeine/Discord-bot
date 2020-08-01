ab08138118b05320eb5a0601eb5ed8c715550d70
KoalaOnCaffeine <tomtomnewton2005@gmail.com>
2020-08-01 01:55:09 +0100

    Implemented the guild saving to a database when a bot joins it for the first time (or more accurately when the guild document isn't in the database)

--- src/main/java/me/tomnewton/discordbot/listeners/GuildJoinListener.kt
+++ src/main/java/me/tomnewton/discordbot/listeners/GuildJoinListener.kt
@@ -1,12 +1,20 @@
 package me.tomnewton.discordbot.listeners
 
 import me.tomnewton.discordbot.database.Database
+import me.tomnewton.discordbot.database.Model
+import me.tomnewton.discordbot.database.models.DefaultGuildModel
+import me.tomnewton.discordbot.registries.Registry
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-class GuildJoinListener(private val database: Database) : ListenerAdapter() {
+class GuildJoinListener(private val database: Database, private val guilds: Registry<Long, Model>) : ListenerAdapter() {
 
     override fun onGuildJoin(event: GuildJoinEvent) {
-        // TODO add to database
+        val id = event.guild.idLong
+        if (!guilds.contains(id)) {
+            val newGuildModel = DefaultGuildModel(id)
+            guilds.register(newGuildModel)
+            database.save(newGuildModel)
+        }
     }
 }
